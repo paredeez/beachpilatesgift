da76071fca6d1f8fce76d6261afd35665089b6fe
Samuel Paredes <samparedes8802@gmail.com>
2025-11-18 14:05:07 +1100

    header change

--- app/page.js
+++ app/page.js
@@ -130,7 +130,7 @@ export default function Home() {
           </div>
 
           <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-center mb-3 md:mb-4 text-gray-800 font-[family-name:var(--font-playfair)]">
-            Beach Pilates Gift Vouchers
+            Beach Pilates Melbourne Gift Vouchers
           </h1>
           <p className="text-base md:text-lg lg:text-xl text-gray-700 text-center max-w-2xl mb-3 md:mb-4 px-2">
             Give the gift of wellness and movement by the beach
